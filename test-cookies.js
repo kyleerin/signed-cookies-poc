e506e73600e4739e23c0732f80aa8387fc31f9c8
kyle <kphillips@infoblox.com>
2020-07-24 17:34:34 -0700

    removed unneeded packages

--- test-cookies.js
+++ test-cookies.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 
 const {exit, exec} = require("shelljs")
-const axios = require('axios').default
-const expire = 1598306437
-const cdnDomain = 'https://d1eztk77tajaox.cloudfront.net'
-const keypair = 'APKAIZLP3CRFM7XPHETA'
+const expire = 1598306437 // <--- make sure this is the same as the number in your policy.json
+const cdnDomain = 'https://d1eztk77tajaox.cloudfront.net' // <--- change this to your cloudfront url, and update ./policy.json as well
+const keypair = 'APKAIZLP3CRFM7XPHETA' // <--- change this to your cloudfront keypairid (taken from account security settings)
 
 // sign policy
 cmd = `cat policy.json | tr -d "\\n" | tr -d " \\t\\n\\r" | openssl sha1 -sign private.pem | openssl base64 | tr -- '+=/' '-_~'`
@@ -16,7 +15,6 @@ if (res.code !== 0) {
 
 cmd = `curl -b 'CloudFront-Signature=${res.stdout.replace(/\n/g, '')}; CloudFront-Expires=${expire}; CloudFront-Key-Pair-Id=${keypair}; Secure; HttpOnly' ${cdnDomain}/bucket.js`
 res = exec(cmd, {silent: true})
-// console.log(cmd)
 if (res.code !== 0) {
   console.log('error making request', res.error)
   exit(1)
